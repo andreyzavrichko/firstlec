32ffa7fc65ac207e30f0a536dbd72d29c4d1952a
andreyzavrichko <andrey@zavrichko.dev>
2019-11-23 14:02:43 +0200

    fourth point

--- 4.php
+++ 4.php
@@ -3,10 +3,10 @@
 $day = '5';
 echo 'Номер дня недели: ' .$day .PHP_EOL;
 switch ($day) {
-    case $day >=1 && $day <=5;
+    case $day >=1 && $day <=5:
         echo 'Это рабочий день';
         break;
-    case $day == 6 || $day == 7;
+    case $day == 6 || $day == 7:
         echo 'Это выходной день';
         break;
     default:

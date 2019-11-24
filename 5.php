97af8aeeb1b086542d2ab3e8a7ffb1efea380827
andreyzavrichko <andrey@zavrichko.dev>
2019-11-24 10:54:11 +0200

    update tag pre and add foreach

--- 5.php
+++ 5.php
@@ -18,14 +18,14 @@ $mercedes = array(
     'doors' => 5,
     'year' => '2014'
 );
-$car['bmw'] = array('X5', 120, 5, '2015');
-$car['audi'] = array('A6', 100, 5, '2000');
-$car['mercedes'] = array('S320', 150, 5, '2014');
-$name = array_keys($car);
-echo 'CAR ' .$name[0] .PHP_EOL .$car['bmw'][0] .' '.$car['bmw'][1].' '.$car['bmw'][2].' '.$car['bmw'][3]
-    .PHP_EOL.'<br>';
-echo 'CAR ' .$name[1] .PHP_EOL .$car['audi'][0] .' '.$car['audi'][1].' '.$car['audi'][2].' '.$car['audi'][3]
-    .PHP_EOL.'<br>';
-echo 'CAR ' .$name[2] .PHP_EOL .$car['mercedes'][0] .' '.$car['mercedes'][1].' '.$car['mercedes'][2].' '
-    .$car['mercedes'][3] .PHP_EOL;
+$car = ['bmw' => $bmw, 'audi' => $audi, 'mercedes' => $mercedes];
+foreach ($car as $name => $carInfo);
+{
+    echo 'CAR'. ' ' .'bmw'. PHP_EOL;
+    echo $bmw['model'] . ' ' . $carInfo['speed'] . ' ' . $carInfo['doors'] . ' ' . $carInfo['year'] . PHP_EOL;
+    echo 'CAR'. ' ' .'audi'. PHP_EOL;
+    echo $audi['model'] . ' ' . $carInfo['speed'] . ' ' . $carInfo['doors'] . ' ' . $carInfo['year'] . PHP_EOL;
+    echo 'CAR'. ' ' .'mercedes'. PHP_EOL;
+    echo $mercedes['model'] . ' ' . $carInfo['speed'] . ' ' . $carInfo['doors'] . ' ' . $carInfo['year'] . PHP_EOL;
+}
 echo '</pre>';
